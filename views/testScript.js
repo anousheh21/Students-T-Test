d77bb92eaae49a1c4e47ea8c58725d28e12438cf
anousheh21 <anousheh1@gmail.com>
2023-06-28 07:40:50 +0100

    Dynamically added input boxes corresponding to the sample size indicated by the user

--- views/testScript.js
+++ views/testScript.js
@@ -1,18 +1,34 @@
+// Variables
+
 const buttonA = document.querySelector('.asubmit')
 const buttonB = document.querySelector('.bsubmit')
 const subheadingA = document.querySelector('.asubheading')
 const subheadingB = document.querySelector('.bsubheading')
 const inputA = document.querySelector('.ainput')
 const inputB = document.querySelector('.binput')
+const addItemsA = document.getElementById('addItemsA')
+const addItemsB = document.getElementById('addItemsB')
+
+// Main
 
 buttonA.addEventListener('click', () => {
     let a = inputA.value;
     if(a=='') {
         subheadingA.textContent = 'Values Required'
-    } else if (a < 5 || a > 15) {
+    } else if (a < 3 || a > 15) {
         subheadingA.textContent = 'Invalid Sample Size'
     } else {
         subheadingA.textContent = 'Data Set';
+        inputA.remove();
+        buttonA.remove();
+        for(i=0; i<a; i++) {
+            let input = document.createElement("input")
+            input.type = "number"
+            input.name = "aValue" + i
+            input.classList.add("aValue", "dataValueInput")
+            addItemsA.appendChild(input)
+            addItemsA.appendChild(document.createElement("br"))
+        }
     }
     
 })
@@ -21,9 +37,20 @@ buttonB.addEventListener('click', () => {
     let b = inputB.value;
     if(b=='') {
         subheadingB.textContent = 'Values Required'
-    } else if (b < 5 || b > 15) {
+    } else if (b < 3 || b > 15) {
         subheadingB.textContent = 'Invalid Sample Size'
     } else {
         subheadingB.textContent = 'Data Set';
+        inputB.remove();
+        buttonB.remove();
+        for(i=0; i<b; i++) {
+            let input = document.createElement("input")
+            input.type = "number"
+            input.name = "bValue" + i
+            input.classList.add("bValue", "dataValueInput")
+            addItemsB.appendChild(input)
+            addItemsB.appendChild(document.createElement("br"))
+        }
     }
 })
+
