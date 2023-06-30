cb53ead25b6226c29d3d3bd9815f1158d36138e1
anousheh21 <anousheh1@gmail.com>
2023-06-30 07:57:07 +0100

    Completed Calculations and Hypothesis Test

--- views/resultsScript.js
+++ views/resultsScript.js
@@ -11,9 +11,13 @@ let sizeB = localStorage.getItem('sampleSizeB')
 let sizeA = localStorage.getItem('sampleSizeA')
 
 // Other Variables
-const displayResults = document.getElementById('results')
+let cv = 0
+const displayTValue = document.getElementById('tValue')
+const displayDF = document.getElementById('df')
+const displayCV = document.getElementById('cv')
+let displayCompare = document.getElementById('compare')
 
-// Calculations
+// t - value Calculations
 let df = (sizeA - 1) + (sizeB - 1)
 let numerator = meanAValue - meanBValue
 let denom1 = (1/sizeA) + (1/sizeB)
@@ -25,5 +29,74 @@ let denominatorSquare = denom1 * denom2 * denom3
 let denominator = Math.sqrt(denominatorSquare)
 let t = numerator/denominator
 
-displayResults.textContent = t
+// Critical Value Calculations
+if (df == 4) {
+    cv = 2.776
+} else if (df == 5) {
+    cv = 2.571
+} else if (df == 6) {
+    cv = 2.447
+} else if (df == 5) {
+    cv = 2.571
+} else if (df == 6) {
+    cv = 2.447
+} else if (df == 7) {
+    cv = 2.365
+} else if (df == 8) {
+    cv = 2.306
+} else if (df == 9) {
+    cv = 2.262
+} else if (df == 10) {
+    cv = 2.228
+} else if (df == 11) {
+    cv = 2.201
+} else if (df == 12) {
+    cv = 2.179
+} else if (df == 13) {
+    cv = 2.160
+} else if (df == 14) {
+    cv = 2.145
+} else if (df == 15) {
+    cv = 2.131
+} else if (df == 16) {
+    cv = 2.120
+} else if (df == 17) {
+    cv = 2.110
+} else if (df == 18) {
+    cv = 2.101
+} else if (df == 19) {
+    cv = 2.093
+} else if (df == 20) {
+    cv = 2.086
+} else if (df == 21) {
+    cv = 2.080
+} else if (df == 22) {
+    cv = 2.074
+} else if (df == 23) {
+    cv = 2.069
+} else if (df == 24) {
+    cv = 2.064
+} else if (df == 25) {
+    cv = 2.060
+} else if (df == 26) {
+    cv = 2.056
+} else if (df == 27) {
+    cv = 2.052
+} else if (df == 28) {
+    cv = 2.048
+} else {
+    cv = 'Error'
+}
+
+// Comparing Calculated T-Value With Critical Value
+if (Math.abs(t) > cv) {
+    displayCompare.textContent = `Reject H0. Significant difference between the two data sets`
+} else if (Math.abs(t) < cv) {
+    displayCompare.textContent = `Fail to reject H0. No significant difference between the two data sets.`
+}
+
+// Display
+displayTValue.textContent = 't-value: ' + t
+displayDF.textContent = 'df: ' + df
+displayCV.textContent = 'critical value: ' + cv
 
