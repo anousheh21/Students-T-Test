df47245a956836d52a9e39d8342516e018705b10
anousheh21 <anousheh1@gmail.com>
2023-06-30 16:16:51 +0100

    Clears local storage before each new t-test is carried out

--- views/script.js
+++ views/script.js
@@ -2,6 +2,7 @@ const button = document.querySelector('.enterButton')
 const testUrl = 'http://localhost:3000/sampleA.html'
 
 button.addEventListener('click', () => {
+    window.localStorage.clear();
     window.location.assign(testUrl)
 })
 
